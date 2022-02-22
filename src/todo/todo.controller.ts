e5f0296a0a88e6612ee630d7a2adbdb41ebe1865
mu0363 <dde036@gmail.com>
2022-02-22 11:05:33 +0900

    add: Todoコントローラーのルーティングを仮実装

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 @Controller('todos')
-export class TodoController {}
+export class TodoController {
+  @Get()
+  findAll() {
+    return 'find all';
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    console.log(body);
+
+    return 'created';
+  }
+
+  @Post(':todoId/duplicate')
+  duplicate(@Param('todoId') todoId: string) {
+    return `todo duplicated with ${todoId}`;
+  }
+
+  @Put('toggle')
+  toggleDone() {
+    return 'toggled status';
+  }
+
+  @Put('order')
+  updateOrder() {
+    return 'update order';
+  }
+
+  @Put(':todoId')
+  updateContent(@Param('todoId') todoId: string) {
+    return `updated content with ${todoId}`;
+  }
+
+  @Delete(':todoId')
+  remove(@Param('todoId') todoId: string) {
+    return `deleted todo with ${todoId}`;
+  }
+}
