480eff6436edeb6987f279d02431c373706f0548
mu0363 <dde036@gmail.com>
2022-02-22 21:12:58 +0900

    add: todoサービスにfindAll・create・toggleDone・deleteを追加

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,23 +1,74 @@
-import { CreateTodoDto } from './dto/todo.dto';
+import { PrismaService } from './../prisma.service';
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { USER_1, USER_2 } from 'src/constant';
 
 @Injectable()
 export class TodoService {
+  constructor(private prisma: PrismaService) {}
   findAll() {
-    return 'find all here';
+    return this.prisma.todo.findMany();
   }
 
-  create(payload: CreateTodoDto) {
-    console.log(payload);
-    return 'created here';
+  async create(payload: Prisma.TodoCreateInput) {
+    const todo = await this.prisma.todo.create({
+      data: {
+        content: payload.content,
+        status: 'TODAY',
+        userId: USER_2,
+      },
+    });
+
+    const todoOrders = await this.prisma.todoOrder.findMany({
+      where: {
+        userId: todo.userId,
+        status: 'TODAY',
+      },
+    });
+
+    if (todoOrders[0].todoIds === '') {
+      return await this.prisma.todoOrder.updateMany({
+        where: {
+          userId: todo.userId,
+          status: 'TODAY',
+        },
+        data: {
+          todoIds: `${todo.id}`,
+        },
+      });
+    } else {
+      const currentTodoIds = todoOrders[0].todoIds.split(',');
+      currentTodoIds.push(String(todo.id));
+      return await this.prisma.todoOrder.updateMany({
+        where: {
+          userId: todo.userId,
+          status: 'TODAY',
+        },
+        data: {
+          todoIds: currentTodoIds.join(','),
+        },
+      });
+    }
   }
 
   duplicate(todoId: number) {
     return `todo duplicated with ${todoId}`;
   }
 
-  toggleDone() {
-    return 'toggled status';
+  async toggleDone(todoId: number) {
+    const todo = await this.prisma.todo.findUnique({
+      where: {
+        id: todoId,
+      },
+    });
+    return this.prisma.todo.update({
+      where: {
+        id: todoId,
+      },
+      data: {
+        done: !todo.done,
+      },
+    });
   }
 
   updateOrder() {
@@ -28,7 +79,11 @@ export class TodoService {
     return `updated content with ${todoId}`;
   }
 
-  remove(todoId: number) {
-    return `deleted todo with ${todoId}`;
+  delete(todoId: number) {
+    return this.prisma.todo.delete({
+      where: {
+        id: todoId,
+      },
+    });
   }
 }
