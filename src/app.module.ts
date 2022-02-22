3f7db3949e646b1dd7f01f2a8b7a68d5e099cbcd
mu0363 <dde036@gmail.com>
2022-02-22 10:21:53 +0900

    add: Todoサービス・コントローラーの作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { healthCheckController } from './healthCheck.controller';
+import { TodoController } from './todo/todo.controller';
+import { TodoService } from './todo/todo.service';
 
 @Module({
   imports: [],
-  controllers: [healthCheckController],
-  providers: [],
+  controllers: [healthCheckController, TodoController],
+  providers: [TodoService],
 })
 export class AppModule {}
