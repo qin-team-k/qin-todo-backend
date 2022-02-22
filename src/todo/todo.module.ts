0530afa3a5c967ca82632892bb5af92e64570047
mu0363 <dde036@gmail.com>
2022-02-22 18:36:59 +0900

    add: prismaサービスをtodoモジュールへ追加

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,9 +1,10 @@
+import { PrismaService } from './../prisma.service';
 import { Module } from '@nestjs/common';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
 
 @Module({
   controllers: [TodoController],
-  providers: [TodoService],
+  providers: [TodoService, PrismaService],
 })
 export class TodoModule {}
