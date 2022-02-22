539c1ca7b51135d5cd75d1987727c9842f8d607e
mu0363 <dde036@gmail.com>
2022-02-22 10:27:45 +0900

    add: Todoモジュールを作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { healthCheckController } from './healthCheck.controller';
-import { TodoController } from './todo/todo.controller';
-import { TodoService } from './todo/todo.service';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
-  controllers: [healthCheckController, TodoController],
-  providers: [TodoService],
+  imports: [TodoModule],
+  controllers: [healthCheckController],
 })
 export class AppModule {}
