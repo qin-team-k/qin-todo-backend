d253dee5fad54bb8442deb2f98a99d7d97fa81a9
mu0363 <dde036@gmail.com>
2022-02-28 11:12:38 +0900

    refactor: 現在使用していないuser module, service, controllerを削除

--- src/api/todo/dto/todo.dto.ts
+++ src/api/todo/dto/todo.dto.ts
@@ -1,9 +1,13 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class CreateTodoDto {
   @IsString()
+  @IsNotEmpty()
+  @MaxLength(200)
   content: string;
 
   @IsString()
+  @IsNotEmpty()
+  @MaxLength(8)
   status: string;
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TodoModule } from './api/todo/todo.module';
-import { UserModule } from './api/user/user.module';
 import { healthCheckController } from './healthCheck.controller';
 
 @Module({
-  imports: [TodoModule, UserModule],
+  imports: [TodoModule],
   controllers: [healthCheckController],
 })
 export class AppModule {}
