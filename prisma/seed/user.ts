a101c5b190c3ecf8f6061328296916cb22b5aeb5
mu0363 <dde036@gmail.com>
2022-02-28 16:40:10 +0900

    fix: seed内ユーザーデータのメールアドレスを変更

--- prisma/seed/user.ts
+++ prisma/seed/user.ts
@@ -14,14 +14,14 @@ export const user = async () => {
       {
         id: '62ac362a-cd60-45b1-9100-3b469eceb31d',
         username: 'Sam Smith',
-        email: 'sam@yahoo.co.jp',
+        email: 'sam@example.com',
         avatarUrl:
           'https://gravatar.com/avatar/74c7e4e5834719babfb22bb40fe20e1b?s=400&d=robohash&r=x',
       },
       {
         id: '7d564bce-2b98-4a86-b5b3-a942e6241584',
         username: 'Tomas Cruise',
-        email: 'tom@gmail.com',
+        email: 'tom@example.com',
         avatarUrl:
           'https://gravatar.com/avatar/2712b6493c3684cb6f7c5c0ebd580a37?s=400&d=robohash&r=x',
       },
