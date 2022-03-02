aa1e1cc3e9aed802ca983a99c7d8bbfdb732f9ff
mu0363 <dde036@gmail.com>
2022-03-02 17:49:55 +0900

    refactor: idをcreatedTodo.idへ変更

--- src/api/todo/dto/create-todo.dto.ts
+++ src/api/todo/dto/create-todo.dto.ts
@@ -1,10 +1,6 @@
 import { IsNotEmpty, IsString, MaxLength, IsNumber } from 'class-validator';
 
 export class CreateTodoDto {
-  @IsNumber()
-  @IsNotEmpty()
-  id?: number;
-
   // FIXME カスタムバリデーション追加
   @IsNotEmpty()
   status: 'TODAY' | 'TOMORROW' | 'NEXT';
