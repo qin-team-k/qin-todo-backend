a0701c56a3f817049dcf4aa309e26baa0f3a3705
mu0363 <dde036@gmail.com>
2022-03-02 12:18:53 +0900

    refactor: 不要なフィールド削除・validation追加

--- src/api/todo/dto/create-todo.dto.ts
+++ src/api/todo/dto/create-todo.dto.ts
@@ -1,16 +1,15 @@
-import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString, MaxLength } from 'class-validator';
 
 export class CreateTodoDto {
+  @IsNumber()
+  @IsNotEmpty()
   id?: number;
+
   // FIXME カスタムバリデーション追加
   status: 'TODAY' | 'TOMORROW' | 'NEXT';
-  done?: boolean;
 
   @IsString()
   @IsNotEmpty()
   @MaxLength(200)
   content: string;
-
-  createdAt?: Date;
-  updatedAt?: Date;
 }
