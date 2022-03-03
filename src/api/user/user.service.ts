3cda857285b28173b1e7e5d634ad0f118d82bce5
mu0363 <dde036@gmail.com>
2022-03-03 19:45:07 +0900

    feat: add User and Auth module controller service

---
+++ src/api/user/user.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
+import { UserDto } from './dto/user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(private prismaService: PrismaService) {}
+  async createUser(userBody: UserDto): Promise<User> {
+    const url =
+      'https://gravatar.com/avatar/4eb616d78c7f80ce3924e825a55e6b73?s=400&d=robohash&r=x';
+    return this.prismaService.user.create({
+      data: {
+        id: '123',
+        username: userBody.username,
+        email: userBody.email,
+        avatarUrl: userBody.avatarUrl,
+      },
+    });
+  }
+
+  getUser() {
+    return 'get user';
+  }
+
+  deleteUser() {
+    return 'delete user';
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './api/auth/auth.module';
+
 import { TodoModule } from './api/todo/todo.module';
+import { UserModule } from './api/user/user.module';
 import { healthCheckController } from './healthCheck.controller';
 
 @Module({
-  imports: [TodoModule],
+  imports: [TodoModule, AuthModule, UserModule],
   controllers: [healthCheckController],
 })
 export class AppModule {}
