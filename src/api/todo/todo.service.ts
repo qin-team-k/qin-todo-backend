5b48e36db56ea8f8500c1b41e250346a06935d8c
mu0363 <dde036@gmail.com>
2022-03-03 11:18:26 +0900

    fix: import順を自動修正

--- src/api/todo/todo.service.ts
+++ src/api/todo/todo.service.ts
@@ -1,10 +1,10 @@
-import { UpdateTodoDto } from './dto/update-todo.dto';
 import { Injectable } from '@nestjs/common';
+import { Todo } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoOrderDto } from './dto/update-todo-order.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
 import { FindAllRes } from './response/findAll.response.';
-import { Todo } from '@prisma/client';
 
 @Injectable()
 export class TodoService {
