f7f62f4792e9abe0380d474075bc77e8233597bc
mu0363 <dde036@gmail.com>
2022-03-04 13:18:04 +0900

    feat: add serializer to authModule

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { GoogleStrategy } from 'src/common/strategies/google.strategy';
 import { PrismaService } from 'src/prisma.service';
+import { SessionSerializer } from './Serializer';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { GoogleStrategy } from './strategies';
 
 @Module({
   controllers: [AuthController],
-  providers: [GoogleStrategy, AuthService, PrismaService],
+  providers: [GoogleStrategy, SessionSerializer, AuthService, PrismaService],
 })
 export class AuthModule {}
