83e7c666fe77ba4ae9035d72b48f493cf31263eb
magako <qutianh@gmail.com>
2022-03-05 00:08:32 +0900

    :bug: fix: setting.json for source eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
     'import/order': [2, { alphabetize: { order: 'asc' } }],
     'unused-imports/no-unused-imports': 'warn',
     'no-console': ['error', { allow: ['warn', 'info', 'error'] }],
