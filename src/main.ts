595319e1310e47c3f212a57a2eb80c623cbc5c04
mu0363 <dde036@gmail.com>
2022-03-04 10:11:18 +0900

    fix: remove fastify

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,9 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
 
