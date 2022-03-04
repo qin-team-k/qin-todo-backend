0e74e6aff6a7b57c1f5c4c9d29c1d2f690935eeb
mu0363 <dde036@gmail.com>
2022-03-04 10:45:10 +0900

    fix: remove user module, service, controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { AuthModule } from './api/auth/auth.module';
-
 import { TodoModule } from './api/todo/todo.module';
-import { UserModule } from './api/user/user.module';
 import { healthCheckController } from './healthCheck.controller';
 
 @Module({
-  imports: [
-    TodoModule,
-    AuthModule,
-    UserModule,
-    PassportModule.register({ session: true }),
-  ],
+  imports: [TodoModule, AuthModule, PassportModule.register({ session: true })],
   controllers: [healthCheckController],
 })
 export class AppModule {}
