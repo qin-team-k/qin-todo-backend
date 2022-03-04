ba7417de35fb85a2a8c1c3d6fcc69335615adde0
magako <qutianh@gmail.com>
2022-03-04 23:03:04 +0900

    :rotating_light: add: todo controller spec

--- src/api/todo/todo.controller.spec.ts
+++ src/api/todo/todo.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from 'src/prisma.service';
 import { TodoController } from './todo.controller';
+import { TodoService } from './todo.service';
 
 describe('TodoController', () => {
   let controller: TodoController;
@@ -7,6 +9,7 @@ describe('TodoController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TodoController],
+      providers: [TodoService, PrismaService],
     }).compile();
 
     controller = module.get<TodoController>(TodoController);
