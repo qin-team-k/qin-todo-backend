cb0b083c76348c44033117b171199a95378cf5a1
mu0363 <dde036@gmail.com>
2022-03-04 10:12:40 +0900

    feat: add session and passport into main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as session from 'express-session';
+import * as passport from 'passport';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -21,6 +23,21 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  app.use(
+    session({
+      cookie: {
+        maxAge: 60000 * 60 * 24,
+      },
+      secret: 'aklsdjflaksdfklakslefkl',
+      resave: false,
+      //falseにする理由は、ログインしていないのも関わらずめっちゃたくさんの新しいsessionを作るから
+      saveUninitialized: false,
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   const port = Number(process.env.PORT) || 3000;
   await app.listen(port, '0.0.0.0');
