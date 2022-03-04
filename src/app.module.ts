af33a3e7698612607863a046cf2202a25803aeb6
mu0363 <dde036@gmail.com>
2022-03-04 10:18:00 +0900

    feat: add PassportModule to appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { AuthModule } from './api/auth/auth.module';
 
 import { TodoModule } from './api/todo/todo.module';
@@ -6,7 +7,12 @@ import { UserModule } from './api/user/user.module';
 import { healthCheckController } from './healthCheck.controller';
 
 @Module({
-  imports: [TodoModule, AuthModule, UserModule],
+  imports: [
+    TodoModule,
+    AuthModule,
+    UserModule,
+    PassportModule.register({ session: true }),
+  ],
   controllers: [healthCheckController],
 })
 export class AppModule {}
