9635c2ad80bdcf29cc7cb9ff2340a925582fc371
mu0363 <dde036@gmail.com>
2022-03-04 19:42:07 +0900

    feat: add authGuard in todo controller

--- src/api/todo/todo.controller.ts
+++ src/api/todo/todo.controller.ts
@@ -7,9 +7,13 @@ import {
   ParseIntPipe,
   Post,
   Put,
+  Req,
+  Res,
+  UseGuards,
   Version,
 } from '@nestjs/common';
 import { Todo } from '@prisma/client';
+import { AuthenticatedGuard } from 'src/common/guards/auth.guard';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoOrderDto } from './dto/update-todo-order.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
@@ -23,7 +27,9 @@ export class TodoController {
   // Todo一覧取得
   @Version('1')
   @Get()
-  findAll(): Promise<FindAllRes> {
+  @UseGuards(AuthenticatedGuard)
+  findAll(@Req() req, @Res() res): Promise<FindAllRes> {
+    console.log(req.session.passport.user);
     const userId = '4ff64eb1-c22a-4455-a50d-75cdc3c1e561';
     return this.todoService.findAll(userId);
   }
@@ -31,6 +37,7 @@ export class TodoController {
   // Todo作成
   @Version('1')
   @Post()
+  @UseGuards(AuthenticatedGuard)
   create(@Body() todo: CreateTodoDto): Promise<Todo> {
     const userId = '4ff64eb1-c22a-4455-a50d-75cdc3c1e561';
     return this.todoService.create(userId, todo);
@@ -39,6 +46,7 @@ export class TodoController {
   // Todo複製
   @Version('1')
   @Post(':todoId/duplicate')
+  @UseGuards(AuthenticatedGuard)
   duplicate(@Param('todoId', ParseIntPipe) todoId: number): Promise<Todo> {
     const userId = '4ff64eb1-c22a-4455-a50d-75cdc3c1e561';
     return this.todoService.duplicate(userId, todoId);
@@ -47,6 +55,7 @@ export class TodoController {
   // 完了・未完了の切り替え
   @Version('1')
   @Put(':todoId/toggle')
+  @UseGuards(AuthenticatedGuard)
   toggleDone(@Param('todoId', ParseIntPipe) todoId: number): Promise<Todo> {
     const userId = '4ff64eb1-c22a-4455-a50d-75cdc3c1e561';
     return this.todoService.toggleDone(todoId);
@@ -55,6 +64,7 @@ export class TodoController {
   // Todo並び替え
   @Version('1')
   @Put(':todoId/order')
+  @UseGuards(AuthenticatedGuard)
   updateOrder(
     @Param('todoId', ParseIntPipe) todoId: number,
     @Body() todo: UpdateTodoOrderDto,
@@ -66,6 +76,7 @@ export class TodoController {
   // Todo内容更新
   @Version('1')
   @Put(':todoId')
+  @UseGuards(AuthenticatedGuard)
   updateContent(
     @Param('todoId', ParseIntPipe) todoId: number,
     @Body() todo: UpdateTodoDto,
@@ -77,6 +88,7 @@ export class TodoController {
   // Todo削除
   @Version('1')
   @Delete(':todoId')
+  @UseGuards(AuthenticatedGuard)
   delete(@Param('todoId', ParseIntPipe) todoId: number): Promise<void> {
     const userId = '4ff64eb1-c22a-4455-a50d-75cdc3c1e561';
     return this.todoService.delete(userId, todoId);
