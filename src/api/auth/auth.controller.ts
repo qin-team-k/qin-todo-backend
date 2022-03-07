89fc69a08f2f3bdd7db82c9527a6e51a9698dff8
mu0363 <dde036@gmail.com>
2022-03-07 11:26:30 +0900

    :sparkles: feat: add auth controller routes

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,16 +1,64 @@
-import { Controller, Get, Req, UseGuards, Version } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { AuthService } from './auth.service';
+import {
+  Controller,
+  Get,
+  Req,
+  Res,
+  Session,
+  UseGuards,
+  Version,
+} from '@nestjs/common';
+import {
+  AuthenticatedGuard,
+  GoogleAuthGuard,
+} from 'src/common/guards/auth.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  /**
+   * GET /api/v1/auth/login
+   * username passwordで登録する場合はPOSTだけどOAuthの場合は違う
+   * ユーザーが認証でくるところ。OAuthプロバイダーにリダイレクトする。
+   * その後認証の確認ボタンを押す。どうやって押したことを知るのか？
+   * OAuthプロバイダーはこちら側の redirect URL を呼び出す。(コールバック)
+   */
+  @Version('1')
+  @Get('login')
+  @UseGuards(GoogleAuthGuard)
+  login() {
+    return;
+  }
+  /**
+   * GET /api/v1/auth/callback
+   * OAuthプロバイダーが認証後に呼び出すredirect URL
+   */
+  @Version('1')
+  @Get('callback')
+  @UseGuards(GoogleAuthGuard)
+  redirect(@Res() res) {
+    res.redirect('http://localhost:8080');
+  }
 
-  // Todo一覧取得
+  /**
+   * GET /api/v1/auth/profile
+   * ユーザー情報を返す
+   */
   @Version('1')
   @Get('profile')
-  @UseGuards(AuthGuard('jwt'))
-  profile(@Req() req) {
-    return 'ok';
+  @UseGuards(AuthenticatedGuard)
+  status(@Req() req) {
+    return { ...req.user };
+  }
+
+  /**
+   * GET /api/v1/auth/logout
+   * ログアウト
+   */
+  @Version('1')
+  @Get('logout')
+  @UseGuards(AuthenticatedGuard)
+  logout(@Session() session, @Res() res) {
+    session.destroy(() => {
+      res.redirect('http://localhost:8080/login');
+    });
   }
 }
