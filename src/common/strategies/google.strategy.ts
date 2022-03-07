2ec07325869c37509877be478a0af6af5e95e3fb
mu0363 <dde036@gmail.com>
2022-03-07 11:22:18 +0900

    :sparkles: feat: add validate

--- src/common/strategies/google.strategy.ts
+++ src/common/strategies/google.strategy.ts
@@ -1,30 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { Profile, Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { AuthService } from 'src/api/auth/auth.service';
 
+/**
+ * この関数は基本的に内部で呼び出され処理される。
+ * AuthModuleのprovidersに登録するだけでOK!!
+ * superはPassportStrategy内のconstructorを実行するために必要。
+ * PassportStrategyはvalidateメソッドを持っている
+ * Passportは...
+ * 1. 認証のステップ（ログインなど）を実行する必要がある
+ * 2. その後ユーザーの資格情報を確認する必要がある(ユーザーのセッションが有効かどうか)
+ * どうやってこれを呼び出すのか? AuthGardにあるloginメソッドを利用する
+ */
 @Injectable()
-export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
-  constructor() {
+export class GoogleStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
     super({
-      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.GOOGLE_CALLBACK_URL,
-      scope: [],
+      scope: ['email', 'profile'],
     });
   }
 
+  /**
+   * 絶対にないとダメ！
+   * もしないとUseGuard()使ったときに「ないですよ」と怒られる
+   * validateの括弧内はgoogleが返してくれるユーザー情報
+   */
   async validate(
     accessToken: string,
     refreshToken: string,
-    profile: any,
+    profile: Profile,
     done: VerifyCallback,
   ): Promise<any> {
-    const { name, emails, photos } = profile;
-    const user = {
-      username: name,
+    const { id, emails, photos, _json, provider } = profile;
+    const googleUserDetails = {
+      id,
       email: emails[0].value,
+      username: _json.name,
+      avatarUrl: photos[0].value,
+      provider,
+      accessToken,
     };
 
+    const user = await this.authService.validateUser(googleUserDetails);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    // nullは成功の意味、userをrequestへ渡す(request.user)
     done(null, user);
   }
 }
