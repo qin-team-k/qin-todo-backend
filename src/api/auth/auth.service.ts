f91ea62c7e2dc9da81d8e676f771d8e5624f1ae7
mu0363 <dde036@gmail.com>
2022-03-07 11:24:06 +0900

    :sparkles: feat: add validateUser

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
+import { GoogleUserDetails } from 'src/types';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  async validateUser(googleUserDetails: GoogleUserDetails): Promise<User> {
+    const { email } = googleUserDetails;
+
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (user) return user;
+    return await this.createUser(googleUserDetails);
+  }
+
+  async createUser(googleUserDetails: GoogleUserDetails): Promise<User> {
+    const user = await this.prisma.user.create({
+      data: {
+        username: googleUserDetails.username,
+        email: googleUserDetails.email,
+        avatarUrl: googleUserDetails.avatarUrl,
+      },
+    });
+    await this.prisma.todoOrder.createMany({
+      data: [
+        { userId: user.id, status: 'TODAY' },
+        { userId: user.id, status: 'TOMORROW' },
+        { userId: user.id, status: 'NEXT' },
+      ],
+    });
+    return user;
+  }
+
+  async findUser(
+    googleUserDetails: GoogleUserDetails,
+  ): Promise<User | undefined> {
+    return await this.prisma.user.findUnique({
+      where: { email: googleUserDetails.email },
+    });
+  }
+}
