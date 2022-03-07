48ec5803e9009334dd73d4fac05bddec4f78c3dc
mu0363 <dde036@gmail.com>
2022-03-07 13:14:51 +0900

    :wastebucket: chore: remove session line

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import * as session from 'express-session';
-import * as passport from 'passport';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -24,27 +22,6 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-
-  app.use(
-    session({
-      cookie: {
-        maxAge: 60000 * 60 * 24,
-      },
-      // FIXME 環境変数に入れる
-      secret: 'aklsdjflaksdfklakslefkl',
-      resave: false,
-      //falseにする理由は、ログインしていないのも関わらずめっちゃたくさんの新しいsessionを作るから
-      saveUninitialized: false,
-      // store: new PrismaSessionStore(new PrismaClient(), {
-      //   checkPeriod: 2 * 60 * 1000, //ms
-      //   dbRecordIdIsSessionId: true,
-      //   dbRecordIdFunction: undefined,
-      // }),
-    }),
-  );
-
-  app.use(passport.initialize());
-  app.use(passport.session());
 
   const port = Number(process.env.PORT) || 3000;
   await app.listen(port, '0.0.0.0');
