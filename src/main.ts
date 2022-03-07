ff0ed53e391741192066d555e3d6ef8814930c0d
mu0363 <dde036@gmail.com>
2022-03-07 11:57:57 +0900

    :sparkles: feat: add express-session

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+import * as session from 'express-session';
+import * as passport from 'passport';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
 
@@ -19,7 +14,8 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: '*',
+    origin: true,
+    credentials: true,
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
   });
 
@@ -28,6 +24,27 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  app.use(
+    session({
+      cookie: {
+        maxAge: 60000 * 60 * 24,
+      },
+      // FIXME 環境変数に入れる
+      secret: 'aklsdjflaksdfklakslefkl',
+      resave: false,
+      //falseにする理由は、ログインしていないのも関わらずめっちゃたくさんの新しいsessionを作るから
+      saveUninitialized: false,
+      // store: new PrismaSessionStore(new PrismaClient(), {
+      //   checkPeriod: 2 * 60 * 1000, //ms
+      //   dbRecordIdIsSessionId: true,
+      //   dbRecordIdFunction: undefined,
+      // }),
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   const port = Number(process.env.PORT) || 3000;
   await app.listen(port, '0.0.0.0');
