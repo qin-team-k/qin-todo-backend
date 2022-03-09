f8e1a9db5f57cd8b84dec80652d0a48add4d32fb
mu0363 <dde036@gmail.com>
2022-03-09 09:49:50 +0900

    :sparkles: feat: add JWT strategy

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from 'src/common/strategies/jwt.strategy';
 import { PrismaService } from 'src/prisma.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -8,6 +9,6 @@ import { AuthService } from './auth.service';
 @Module({
   imports: [PassportModule, JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService, PrismaService],
+  providers: [AuthService, PrismaService, JwtStrategy],
 })
 export class AuthModule {}
