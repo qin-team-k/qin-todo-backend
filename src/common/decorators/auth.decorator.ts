0c29a51a6165a0ff3029890d9916e73097b94088
mu0363 <dde036@gmail.com>
2022-03-09 19:59:26 +0900

    :shower: refactor: delete auth.decorator.ts

--- src/common/decorators/auth.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const GetCurrentUser = createParamDecorator(
-  (data: string | undefined, context: ExecutionContext) => {
-    const request = context.switchToHttp().getRequest();
-    return request.user;
-  },
-);
