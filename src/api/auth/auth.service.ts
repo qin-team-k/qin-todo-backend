3297a7259f4203608b84139bb023e08969081081
mu0363 <dde036@gmail.com>
2022-03-09 11:35:00 +0900

    :shower: chore: remove unused lines

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -13,33 +13,6 @@ export class AuthService {
       where: { email },
     });
 
-    if (user) return user;
-
-    return await this.createUser(googleUserDetails);
-  }
-
-  async createUser(googleUserDetails): Promise<User> {
-    const user = await this.prisma.user.create({
-      data: {
-        username: googleUserDetails.username,
-        email: googleUserDetails.email,
-        avatarUrl: googleUserDetails.avatarUrl,
-      },
-    });
-    await this.prisma.todoOrder.createMany({
-      data: [
-        { userId: user.id, status: 'TODAY' },
-        { userId: user.id, status: 'TOMORROW' },
-        { userId: user.id, status: 'NEXT' },
-      ],
-    });
     return user;
-  }
-
-  async logout(userId: string) {
-    await this.prisma.user.update({
-      where: { id: userId },
-      data: { refreshToken: null },
-    });
   }
 }
--- src/common/interceptors/auth/auth.interceptor.ts
+++ src/common/interceptors/auth/auth.interceptor.ts
@@ -4,14 +4,24 @@ import {
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
+import * as admin from 'firebase-admin';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthInterceptor implements NestInterceptor {
+  /**
+   * context: 入ってくるrequest情報
+   * next: responseで出てく時使う(next.handle().pine())
+   */
   async intercept(
     context: ExecutionContext,
     next: CallHandler,
   ): Promise<Observable<any>> {
+    const ctx = context.switchToHttp();
+    const token = ctx.getRequest().headers['authorization'].split(' ')[1];
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    console.log(decodedToken);
+
     return next.handle();
   }
 }
