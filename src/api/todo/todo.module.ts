91fb2b434804e70754b47b679e49e79428e4d63c
mu0363 <dde036@gmail.com>
2022-03-11 11:36:56 +0900

    :sparkles: import UserModule to TodoModule

--- src/api/todo/todo.module.ts
+++ src/api/todo/todo.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
+import { UserModule } from '../auth/user.module';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
 
 @Module({
+  imports: [UserModule],
   controllers: [TodoController],
   providers: [TodoService, PrismaService],
 })
