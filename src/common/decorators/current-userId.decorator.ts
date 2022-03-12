21b522ba6ba05631d723560141f1d1104bac9728
mu0363 <dde036@gmail.com>
2022-03-12 15:17:34 +0900

    :shower: refactor: delete GetCurrentUserId

--- src/common/decorators/current-userId.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const GetCurrentUserId = createParamDecorator(
-  (_data: string | undefined, context: ExecutionContext) => {
-    const request = context.switchToHttp().getRequest();
-    return request.user.id;
-  },
-);
