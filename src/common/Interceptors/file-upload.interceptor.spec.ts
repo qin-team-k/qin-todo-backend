a8881b2cf0a1c5559cca3b1eb12a694dbae7a75d
mu0363 <dde036@gmail.com>
2022-03-15 16:09:15 +0900

    :shower: remove unused interceptor

--- src/common/Interceptors/file-upload.interceptor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { FileUploadInterceptor } from './file-upload.interceptor';
-
-describe('FileUploadInterceptor', () => {
-  it('should be defined', () => {
-    expect(new FileUploadInterceptor()).toBeDefined();
-  });
-});
