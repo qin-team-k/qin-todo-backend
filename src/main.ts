e1fe4f9777be280d9f0563298487a3aac4f5ae3b
mu0363 <dde036@gmail.com>
2022-03-15 16:11:44 +0900

    :shower: remove  fastify

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,11 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
-import multipart from 'fastify-multipart';
 import { ServiceAccount } from 'firebase-admin';
 import * as admin from 'firebase-admin';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-    { cors: true },
-  );
-
-  app.register(multipart);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   // 環境変数の値を指定してfirebase-admin用のConfigオブジェクトを作成
   const adminConfig: ServiceAccount = {
