ef893de9f8c0a21bd543c15a77a4098aa363002c
mu0363 <dde036@gmail.com>
2022-03-18 20:23:57 +0900

    :recycle: refactor: add update username test

--- src/api/user/user.service.spec.ts
+++ src/api/user/user.service.spec.ts
@@ -1,3 +1,4 @@
+import { ForbiddenException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from 'src/prisma.service';
 import { CloudStorageService } from '../cloud-storage/cloud-storage.service';
@@ -6,6 +7,17 @@ import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
+
+  const createUserDto = {
+    email: 'tom@example.com',
+    name: 'tom cruise',
+    avatarUrl:
+      'https://gravatar.com/avatar/ff3bd3d6733b66d9bf8361c8c8b47147?s=400&d=robohash&r=x',
+    uid: '0e7eaf56-1eed-4dea-b791-2d51efaffc59',
+  };
+
+  const createdAt = new Date('2020-06-01T00:00:00.000Z');
+  const updatedAt = new Date('2020-06-01T00:00:00.000Z');
 
   // beforeAll(async () => {
   //   const module: TestingModule = await Test.createTestingModule({
@@ -18,24 +30,36 @@ describe('UserService', () => {
   beforeEach(async () => {
     // mockのUserサービスを作成
     const mockUserService: Partial<UserService> = {
-      initUser: (createUserDto: CreateUserDto) =>
+      initUser: jest.fn((createUserDto: CreateUserDto) =>
         Promise.resolve({
           id: '24b7282f-9d30-450b-8383-758ec0506b4d',
           uid: createUserDto.uid,
           username: createUserDto.name,
           email: createUserDto.email,
           avatarUrl: createUserDto.avatarUrl,
-          createdAt: new Date('2020-06-01T00:00:00.000Z'),
-          updatedAt: new Date('2020-06-01T00:00:00.000Z'),
+          createdAt,
+          updatedAt,
         }),
-      // updateUsername: (userId: string, paramUserId: string, username: string) =>
-      //   Promise.resolve({}),
+      ),
+      updateUsername: jest.fn(
+        (userId: string, paramUserId: string, username: string) =>
+          Promise.resolve({
+            id: userId,
+            uid: createUserDto.uid,
+            username,
+            email: createUserDto.email,
+            avatarUrl: createUserDto.avatarUrl,
+            createdAt,
+            updatedAt,
+          }),
+      ),
     };
 
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         PrismaService,
         CloudStorageService,
+        UserService,
         {
           provide: UserService,
           useValue: mockUserService,
@@ -46,25 +70,28 @@ describe('UserService', () => {
     service = module.get(UserService);
   });
 
-  const createUserDto = {
-    email: 'jane@example.com',
-    name: 'jane doe',
-    avatarUrl:
-      'https://gravatar.com/avatar/ff3bd3d6733b66d9bf8361c8c8b47147?s=400&d=robohash&r=x',
-    uid: '0e7eaf56-1eed-4dea-b791-2d51efaffc58',
-  };
-
   it('can create an instance of user service', async () => {
     expect(service).toBeDefined();
   });
 
   // FIXME todoOrderテーブルが作成されかをテストしたい
-  it('creates a new user with Google auth and get creates todoOrderTable', async () => {
+  it('Normal case: creates a new user with Google auth and get creates todoOrderTable', async () => {
     const user = await service.initUser(createUserDto);
     // initUser後のデータを確認
     expect(user.uid).toEqual(createUserDto.uid);
     expect(user.email).toEqual(createUserDto.email);
     expect(user.username).toEqual(createUserDto.name);
     expect(user.avatarUrl).toEqual(createUserDto.avatarUrl);
+  });
+
+  // FIXME resolveしてしまう
+  it('Normal case: throws an error if userId and paramUserId is not equal when update username', async () => {
+    const userId = '24b7282f-9d30-450b-8383-758ec0506b4d';
+    const paramUserId = '123';
+    const username = 'Jane Doe';
+
+    await expect(
+      service.updateUsername(userId, paramUserId, username),
+    ).rejects.toThrowError(ForbiddenException);
   });
 });
