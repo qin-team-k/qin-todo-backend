2dbe6fee3e38110cd2d93deed70e8ab513f51801
mu0363 <dde036@gmail.com>
2022-03-26 21:02:20 +0900

    :sparkles: feat: create userService mock

---
+++ src/api/user/__mocks__/user.service.ts
@@ -0,0 +1,7 @@
+import { userStub } from '../test/stubs/user.stub';
+
+export const UserService = jest.fn().mockReturnValue({
+  updateUser: jest.fn().mockResolvedValue(userStub()),
+  uploadAvatarImage: jest.fn().mockResolvedValue(userStub()),
+  deleteUser: jest.fn().mockResolvedValue(userStub()),
+});
