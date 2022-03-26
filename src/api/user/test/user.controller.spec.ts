fa5eb1aca436210ee1721930e74f2df812dfe240
mu0363 <dde036@gmail.com>
2022-03-26 21:44:53 +0900

    :sparkles: feat: add tests about users, updateUsers, updateAvatar, and delete

--- src/api/user/test/user.controller.spec.ts
+++ src/api/user/test/user.controller.spec.ts
@@ -1,9 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { User } from '@prisma/client';
 import { UserService } from 'src/api/user/user.service';
 import { AuthenticateGuard } from 'src/common/guards/authenticate/authenticate.guard';
 import { PrismaService } from 'src/prisma.service';
 import { CloudStorageService } from '../../cloud-storage/cloud-storage.service';
+import { UpdateUserDto } from '../dto/update-user.dto';
 import { UserController } from '../user.controller';
+import { userStub } from './stubs/user.stub';
 
 /**
  * jestにuserServiceをmockすることを伝えると、自動的にspy.onされてるらしい。
@@ -39,5 +42,127 @@ describe('UserController', () => {
   // controllerインスタンス作成のテスト
   it('can create an instance of user controller', async () => {
     expect(controller).toBeDefined();
+  });
+
+  // usersのテスト
+  describe('users', () => {
+    describe('when users is called', () => {
+      let user: User;
+
+      beforeEach(async () => {
+        user = await controller.users(userStub());
+      });
+
+      // 戻り値がuserであるか確認
+      it('then it should return a user', () => {
+        expect(user).toEqual(userStub());
+      });
+    });
+  });
+
+  // updateUserのテスト
+  describe('updateUser', () => {
+    describe('then it when updateUser is called', () => {
+      let user: User;
+      let updateUserDto: UpdateUserDto;
+
+      beforeEach(async () => {
+        updateUserDto = {
+          username: 'Jane Smith',
+        };
+        user = await controller.updateUser(
+          userStub(),
+          userStub().id,
+          updateUserDto,
+        );
+      });
+
+      // controllerで呼び出しているuserServiceのupdateUserが引数(userId, paramUserId, createUserDto)で呼ばれたか確認
+      it('should call userService', () => {
+        expect(service.updateUser).toBeCalledWith(
+          userStub().id,
+          userStub().id,
+          updateUserDto,
+        );
+      });
+
+      // 戻り値がuserであるか確認
+      it('then it should return a user', () => {
+        expect(user).toEqual(userStub());
+      });
+    });
+  });
+
+  // updateAvatarのテスト
+  describe('updateAvatar', () => {
+    describe('when updateAvatar is called', () => {
+      let user: User;
+      let avatarImage: Express.Multer.File;
+      let multer;
+
+      // アップデートファイルのmockを作成
+      jest.mock('multer', () => {
+        multer = () => ({
+          any: () => {
+            return (req, _res, next) => {
+              req.body = { userName: 'testUser' };
+              req.files = [
+                {
+                  originalname: 'sample.name',
+                  mimetype: 'sample.type',
+                  path: 'sample.url',
+                  buffer: Buffer.from('whatever'),
+                },
+              ];
+              return next();
+            };
+          },
+        });
+        multer.memoryStorage = () => jest.fn();
+        return multer;
+      });
+
+      beforeEach(async () => {
+        avatarImage = multer;
+        user = await controller.updateAvatar(
+          userStub(),
+          userStub().id,
+          avatarImage,
+        );
+      });
+
+      // controllerで呼び出しているuserServiceのuploadAvatarImageが引数(userId, paramUserId, createUserDto)で呼ばれたか確認
+      it('then it should call userService', () => {
+        expect(service.uploadAvatarImage).toBeCalledWith(
+          userStub().id,
+          userStub().id,
+          avatarImage,
+        );
+      });
+
+      // 戻り値がuserであるか確認
+      it('then it should return a user', () => {
+        expect(user).toEqual(userStub());
+      });
+    });
+  });
+
+  // deleteのテスト
+  describe('delete', () => {
+    describe('when delete is called', () => {
+      beforeEach(async () => {
+        await service.deleteUser(userStub().id);
+      });
+
+      // 戻り値がundefinedであるか確認
+      it('then it should be undefined', async () => {
+        expect(await controller.delete(userStub())).toBeUndefined();
+      });
+
+      // controllerで呼び出しているuserServiceのupdateUserが引数(userId)で呼ばれたか確認
+      it('then it should call userService', () => {
+        expect(service.deleteUser).toBeCalledWith(userStub().id);
+      });
+    });
   });
 });
