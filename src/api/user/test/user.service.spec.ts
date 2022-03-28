840651528061dae2f2490b4e6edb5e21f62ef984
mu0363 <dde036@gmail.com>
2022-03-28 17:31:57 +0900

    :construction: WIP:  creating uploadAvatarImage test

--- src/api/user/test/user.service.spec.ts
+++ src/api/user/test/user.service.spec.ts
@@ -1,3 +1,4 @@
+import { writeFileSync } from 'fs';
 import { ForbiddenException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodoStatus } from '@prisma/client';
@@ -13,26 +14,10 @@ describe('UserService', () => {
   let service: UserService;
   let prisma: PrismaService;
 
-  let createUserDto: CreateUserDto;
-  let updateUserDto: UpdateUserDto;
-
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [PrismaService, CloudStorageService, UserService],
     }).compile();
-
-    // テストデータ
-    createUserDto = {
-      email: 'John@skynet.com',
-      name: 'John Conner',
-      avatarUrl:
-        'https://gravatar.com/avatar/ff3bd3d6733b66d9bf8361c8c8b47147?s=400&d=robohash&r=x',
-      uid: '0e7eaf56-1eed-4dea-b791-2d51efaffc59',
-    };
-
-    updateUserDto = {
-      username: 'Jane Smith',
-    };
 
     service = module.get<UserService>(UserService);
     prisma = module.get<PrismaService>(PrismaService);
@@ -47,44 +32,17 @@ describe('UserService', () => {
     expect(service).toBeDefined();
   });
 
-  // TODO findUserByUidのテスト
-  // describe('findUserByUid', () => {
-  //   it('Normal case: creates a new user', async () => {
-  //     const user = await service.createUser(createUserDto);
-
-  //     // createUser後のデータを確認
-  //     expect(user.uid).toEqual(createUserDto.uid);
-  //     expect(user.email).toEqual(createUserDto.email);
-  //     expect(user.username).toEqual(createUserDto.name);
-  //     expect(user.avatarUrl).toEqual(createUserDto.avatarUrl);
-  //   });
-  /**
-   * createUserのAbNormal
-   *
-   *
-   */
-  // });
-
-  // findUserByUserIdのテスト
-  // describe('FindUserByUid', () => {
-  //   it('Normal case: creates a new user', async () => {
-  //     const user = await service.createUser(createUserDto);
-
-  //     // createUser後のデータを確認
-  //     expect(user.uid).toEqual(createUserDto.uid);
-  //     expect(user.email).toEqual(createUserDto.email);
-  //     expect(user.username).toEqual(createUserDto.name);
-  //     expect(user.avatarUrl).toEqual(createUserDto.avatarUrl);
-  //   });
-  /**
-   * createUserのAbNormal
-   *
-   */
-  // });
-
   // initUserメソッドのテスト
   describe('InitUser', () => {
     it('Normal case: creates a new user with Google auth and get creates todoOrderTable', async () => {
+      const createUserDto: CreateUserDto = {
+        email: 'John@skynet.com',
+        name: 'John Conner',
+        avatarUrl:
+          'https://gravatar.com/avatar/ff3bd3d6733b66d9bf8361c8c8b47147?s=400&d=robohash&r=x',
+        uid: '0e7eaf56-1eed-4dea-b791-2d51efaffc59',
+      };
+
       const user = await service.initUser(createUserDto);
 
       // initUser後のデータを確認
@@ -123,38 +81,18 @@ describe('UserService', () => {
         TodoStatus.NEXT,
       ]);
     });
-
-    // TODO 未実装ためコメントアウト: PrismaClientKnownRequestErrorをinitUserに追加するべき?
-    /**
-     * if(error instanceof PrismaClientKnownRequestError) {
-     *  if(error.code === "P2002") {
-     *    throw new ForbiddenException("User already exists.")
-     *  }
-     * }
-     * throw error
-     */
-    // it('Abnormal case: throws an error if fail to create a new user', async () => {
-    //   // すでに同じemailが存在するテストデータ
-    //   const createInvalidUserDto = {
-    //     email: 'john@example.com',
-    //     name: 'Jane Smith',
-    //     avatarUrl:
-    //       'https://gravatar.com/avatar/ff3bd3d6733b66d9bf8361c8c8b47147?s=400&d=robohash&r=x',
-    //     uid: '0e7eaf56-1eed-4dea-b791-2d51efaffc59',
-    //   };
-    //   try {
-    //     await service.initUser(createUserDto);
-    //   } catch (error) {
-    //     expect(error.status).toBe(403);
-    //   }
-    // });
   });
 
   // updateUserメソッドのテスト
   describe('updateUser', () => {
+    const updateUserDto: UpdateUserDto = {
+      username: 'Jane Smith',
+    };
+
     it('Normal case: Update username', async () => {
-      const userId = '4ff64eb1-c22a-4455-a50d-75cdc3c1e561';
-      const paramUserId = '4ff64eb1-c22a-4455-a50d-75cdc3c1e561';
+      const userId = '62ac362a-cd60-45b1-9100-3b469eceb31d';
+      const paramUserId = '62ac362a-cd60-45b1-9100-3b469eceb31d';
+
       const userBeforeUpdate = await prisma.user.findUnique({
         where: { id: userId },
       });
@@ -175,6 +113,34 @@ describe('UserService', () => {
     });
   });
 
+  // uploadAvatarImageメソッドのテスト
+  describe('uploadAvatarImage', () => {
+    it('Normal case: upload avatar image', async () => {
+      const userId = '62ac362a-cd60-45b1-9100-3b469eceb31d';
+      const paramUserId = '62ac362a-cd60-45b1-9100-3b469eceb31d';
+
+      const img =
+        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0' +
+        'NAAAAKElEQVQ4jWNgYGD4Twzu6FhFFGYYNXDUwGFpIAk2E4dHDRw1cDgaCAASFOffhEIO' +
+        '3gAAAABJRU5ErkJggg==';
+      // プレフィックスを除去しbase64にエンコードされたbytes
+      const data = img.replace(/^data:image\/\w+;base64,/, '');
+      const buffer = Buffer.from(data, 'base64');
+      await writeFileSync(`${__dirname}/image.png`, buffer);
+
+      const userBeforeUpdate = await prisma.user.findUnique({
+        where: { id: userId },
+      });
+
+      // const updatedUser = await service.uploadAvatarImage(
+      //   userId,
+      //   paramUserId,
+      //   multerAvatarImage,
+      // );
+      // expect(userBeforeUpdate.avatarUrl).not.toEqual(updatedUser.avatarUrl);
+    });
+  });
+
   // deleteUserメソッドのテスト
   describe('deleteUser', () => {
     it('Normal case: delete user', async () => {
@@ -184,11 +150,5 @@ describe('UserService', () => {
       const user = await service.findUserByUserId(userId);
       expect(user).toBeNull();
     });
-
-    // TODO 未実装ためコメントアウト: userが存在しない場合のエラー処理を追加するべき?
-    // it('Abnormal case: throws an error if userId and paramUserId is not equal when update username', async () => {
-    //   const userId = '4ff64eb1-c22a-4455-a50d-75cdc3c1e561';
-    //   expect(await service.deleteUser(userId)).toBeUndefined();
-    // });
   });
 });
